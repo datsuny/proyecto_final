e669847b948ee9ea8f51c731d68651bdf11270f1
datsuny <43563262+datsuny@users.noreply.github.com>
2022-10-12 21:48:14 -0600

    first commit

---
+++ Plantilla_Proyecto/Plantilla_Proyecto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Plantilla_Proyecto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
